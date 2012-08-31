707fb3fb96c3b78e56e82979152c6823ad00f7f7
Matt Vickery <matt.d.vickery@gmail.com>
2012-08-31 16:09:52 +0100

    #3 command line script updated.

--- distribution/run.sh
+++ distribution/run.sh
@@ -2,4 +2,6 @@
 
 mkdir -p build
 javac -sourcepath src -classpath 'dependencies/*' -d build src/biz/c24/io/mongodb/fix/application/CreateNewOrderSingles.java
+javac -sourcepath src -classpath 'dependencies/*' -d build src/biz/c24/io/mongodb/fix/application/CreateExecutionReports.java
 java -classpath 'build:src:dependencies/*' biz.c24.io.mongodb.fix.application.CreateNewOrderSingles
+java -classpath 'build:src:dependencies/*' biz.c24.io.mongodb.fix.application.CreateExecutionReports

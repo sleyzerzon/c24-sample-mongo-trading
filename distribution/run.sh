1ff1fcfbcc672ea721cf81247eff2886b8d76b7e
Andrew Elmore <andrew.elmore@incept5.com>
2012-09-10 17:24:18 +0100

    Include README.md in distribution. Fixed class names in run.sh

--- distribution/run.sh
+++ distribution/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p build
-javac -sourcepath src -classpath 'dependencies/*' -d build src/biz/c24/io/mongodb/fix/application/CreateNewOrderSingles.java
-javac -sourcepath src -classpath 'dependencies/*' -d build src/biz/c24/io/mongodb/fix/application/CreateExecutionReports.java
+javac -sourcepath src -classpath 'dependencies/*' -d build src/biz/c24/io/mongodb/fix/application/NewOrderSingleDataLoader.java
+javac -sourcepath src -classpath 'dependencies/*' -d build src/biz/c24/io/mongodb/fix/application/ExecutionReportDataLoader.java
 java -classpath 'build:src:dependencies/*' biz.c24.io.mongodb.fix.application.NewOrderSingleDataLoader
 java -classpath 'build:src:dependencies/*' biz.c24.io.mongodb.fix.application.ExecutionReportDataLoader
